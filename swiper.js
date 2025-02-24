50135bdb156e77875492ffa35ae9bc4aa87b0e8b
shivani <shivaniw69@gmail.com>
2025-02-25 04:24:11 +0530

    html

--- swiper.js
+++ swiper.js
@@ -1,19 +1,29 @@
 
 
 
-let swiper1 = new Swiper(".swiper1", {
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
+// let swiper1 = new Swiper(".swiper1", {
+//     pagination: {
+//       el: ".swiper-pagination",
+//       dynamicBullets: true,
+//     },
+//     navigation:{
+//       nextEl:".swiper-button-next",
+//       prevEl:".swiper-buuton-prev",
+//     },
+//     autoplay:{
+//       delay:2000
+//     }
+//   });
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
-    navigation:{
-      nextEl:".swiper-button-next",
-      prevEl:".swiper-buuton-prev",
-    }
-    // autoplay:{
-    //   delay:2000
-    // }
-  });
+  },
+});
 
   var swiper2 = new Swiper(".swiper2", {
     slidesPerView: 3,
@@ -23,3 +33,12 @@ let swiper1 = new Swiper(".swiper1", {
       clickable: true,
     },
   });
+
+  // let swiper3 = new Swiper("Swiper3", {
+  //   slidesPerView: 3,
+  //   spaceBetween: 30,
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  // });
