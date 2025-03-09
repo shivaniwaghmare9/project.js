6fb4d0b5a4a63561235f9c5e54d9d9b16f9c2d90
shivani <shivaniw69@gmail.com>
2025-03-09 22:56:25 +0530

    new

--- script.js
+++ script.js
@@ -1,49 +1,20 @@
 
 
-VANTA.NET({
-   el: "#contact",
-   mouseControls: true,
-   touchControls: true,
-   gyroControls: false,
-   minHeight: 200.00,
-   minWidth: 200.00,
-   scale: 1.00,
-   scaleMobile: 1.00,
-   color: 0xb03838,
-   backgroundColor: 0x8070d
- })
+// VANTA.NET({
+//    el: "#contact",
+//    mouseControls: true,
+//    touchControls: true,
+//    gyroControls: false,
+//    minHeight: 200.00,
+//    minWidth: 200.00,
+//    scale: 1.00,
+//    scaleMobile: 1.00,
+//    color: 0xb03838,
+//    backgroundColor: 0x8070d
+//  })
 
 
-   function img1(){
-    let changeimg= document.querySelector("#box");
-    let change=document.querySelector("#textcontent");
-    changeimg.style.backgroundImage="url('./project.images/luxury-collection-img-1.jpg5.jpg')";
-     change.innerHTML="<h1>SPORTS CAR</h1>"  
-
-   }
-   function img2(){
-    let changeimg= document.querySelector("#box");
-    changeimg.style.backgroundImage="url('./project.images/luxury-collection-img-2.jpg3.jpg')";
-    let change=document.querySelector("#textcontent");
-    change.innerHTML="<h1>CONVERTIBLE CAR</h1>" 
-    
-   }
-   function img3(){
-    let changeimg= document.querySelector("#box");
-    changeimg.style.backgroundImage="url('./project.images/luxury-collection-img-3.jpg2.jpg')";
-    let change=document.querySelector("#textcontent");
-    change.innerHTML="<h1>SEDAN CAR</h1>"  
-
-   }
-   function img4(){
-    let changeimg= document.querySelector("#box");
-    changeimg.style.backgroundImage="url('./project.images/luxury-collection-img-4.jpg')";
-    let change=document.querySelector("#textcontent");
-    change.innerHTML="<h1>LUXURY CAR</h1>"  
-
-}
-
-
+  
 let clickme=()=>{
     let display1=document.querySelector("#show")
     if(display1.style.display==='none'){
@@ -98,4 +69,12 @@ let sing=()=>{
    location.href="form.html"
 }
 
-
+ let demoss=()=>{
+   location.href="index.html";
+//    Swal.fire({
+//       icon: "success",
+//       title: "Done",
+//       text: "Are u interested !",
+//       footer: '<a href="index.html">Why do I have this issue?</a>'
+//     });
+ }
--- vanta.js
+++ vanta.js
@@ -1,22 +1,9 @@
 
-VANTA.NET({
-   el: "#contact",
-   mouseControls: true,
-   touchControls: true,
-   gyroControls: false,
-   minHeight: 200.00,
-   minWidth: 200.00,
-   scale: 1.00,
-   scaleMobile: 1.00,
-   color: 0xb04747,
-   backgroundColor: 0x161617,
-   points: 11.00,
-   maxDistance: 24.00,
-   spacing: 16.00
- })
+
+
 
  VANTA.NET({
-  el: "#contacts",
+  el: "#contact",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
