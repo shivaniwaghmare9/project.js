cbf5b747bd7c52e18d6095d5bb3923536aa95223
shivani <shivaniw69@gmail.com>
2025-03-10 22:31:52 +0530

    new

--- vanta.js
+++ vanta.js
@@ -2,15 +2,15 @@
 
 
 
- VANTA.NET({
-  el: "#contact",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  color: 0xb03838,
-  backgroundColor: 0x8070d
-})
+//  VANTA.NET({
+//   el: "#contact",
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00,
+//   scale: 1.00,
+//   scaleMobile: 1.00,
+//   color: 0xb03838,
+//   backgroundColor: 0x8070d
+// })
