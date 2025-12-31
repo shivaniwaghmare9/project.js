f8248f2d682c8799498ecc11dc4057fd69bf8397
shivani <shivaniw69@gmail.com>
2025-12-31 23:09:16 +0530

    js

--- swiper.js
+++ swiper.js
@@ -1,31 +1,5 @@
 
 
-// //===================================first swiper(first section)=============================================
-
-// var swiper = new Swiper(".mySwiper", {
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//     renderBullet: function (index, className) {
-//       return '<span class="' + className + '">' + (index + 1) + "</span>";
-//     },
-//    },
-//   autoplay:{
-//     delay:3000
-//   }
-// });
-// //=============================second swiper(sixth section)=============================================
-//   var swiper2 = new Swiper(".swiper2", {
-//     slidesPerView: 3,
-//     spaceBetween: 30,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//   });
-
-  
-
 //===================================first swiper(first section)=============================================
 
 var swiper = new Swiper(".mySwiper", {
